74c58c0ab541c67fa83762b0678e8b266aeb0b1e
Michaelszeng <michaelszeng@gmail.com>
2024-11-17 16:59:47 -0500

    stuck on indicator cost function implementation when using constraint functions that return formulas

--- admm_test_gcs.py
+++ admm_test_gcs.py
@@ -8,7 +8,10 @@ from pydrake.all import (
     eq,
     le,
     ge,
+    logical_and,
 )
+from pydrake.symbolic import max as sym_max
+
 import numpy as np
 
 ################################################################################
@@ -117,132 +120,12 @@ print(f"optimal cost: {result.get_optimal_cost()}")
 ################################################################################
 # GCS solved with ADMM
 ################################################################################
-# Initialize variables to 0
-x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y = 4, 4, -2, 4, 4, 4, 4, -2, -2, 0  # feasible initial guess (so indicator costs don't explode)
+# Initialize variables to feasible initial guesses
+x_1, x_2, x_3 = np.array([4]), np.array([4]), np.array([-2])
+z1_1, z1_2, z2_2, z2_3, z3_1, z3_3 = x_1, x_2, x_2, x_3, x_1, x_3
+y = np.zeros((6,1))
 
-# # Symbolic Expression for Lagrangian
-# x_1_sym = np.array([Variable("x_1")])
-# x_2_sym = np.array([Variable("x_2")])
-# x_3_sym = np.array([Variable("x_3")])
-# z1_1_sym = np.array([Variable("z1_1")])
-# z1_2_sym = np.array([Variable("z1_2")])
-# z2_2_sym = np.array([Variable("z2_2")])
-# z2_3_sym = np.array([Variable("z2_3")])
-# z3_1_sym = np.array([Variable("z3_1")])
-# z3_3_sym = np.array([Variable("z3_3")])
-# y_sym = np.array([Variable("y")])
-
-def indicator_cost(f, x):
-    """
-    Return penalty if f is violated; 0 otherwise.
-
-    f is the constraint function.
-    x is a list of all variables fed to the constraint function.
-    """
-    penalty = 1e6
-    
-
-def augmented_lagrangian(x_1_sym, x_2_sym, x_3_sym, z1_1_sym, z3_1_sym, z1_2_sym, z2_2_sym, z2_3_sym,  z3_3_sym, y):
-    x_sym = np.vstack((x_1_sym, x_2_sym, x_3_sym))
-    z_sym = np.vstack((z1_1_sym, z3_1_sym, z1_2_sym, z2_2_sym, z2_3_sym, z3_3_sym))
-    
-    return x1_cost(x_1_sym) + x2_cost(x_2_sym) + x3_cost(x_3_sym) + x1_x2_cost(z1_1_sym, z1_2_sym) + x2_x3_cost(z2_2_sym, z2_3_sym) + x1_x3_cost(z3_1_sym, z3_3_sym) \
-        + indicator_cost(x1_constraint, [x_1_sym]) + indicator_cost(x2_constraint, [x_2_sym]) + indicator_cost(x3_constraint, [x_3_sym]) \
-        + indicator_cost(x1_x2_constraint, [x_1_sym, x_2_sym]) + indicator_cost(x2_x3_constraint, [x_2_sym, x_3_sym]) + indicator_cost(x1_x3_constraint, [x_1_sym, x_3_sym]) \
-        + np.dot(y, (A @ x_sym + B @ z_sym - c)) \
-        + rho/2 * np.dot((A @ x_sym + B @ z_sym - c), (A @ x_sym + B @ z_sym - c))
-    
-
-def argmin_x1(x_2, x_3, z1_1, z3_1, z1_2, z2_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    x_1 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"x1 update: Is solved successfully: {result.is_success()}")
-    print(f"x1 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"x1 update: x1 optimal value: {result.GetSolution(x_1)}")
-    return result.GetSolution(x_1)
-
-def argmin_x2(x_1, x_3, z1_1, z3_1, z1_2, z2_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    x_2 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"x2 update: Is solved successfully: {result.is_success()}")
-    print(f"x2 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"x2 update: x2 optimal value: {result.GetSolution(x_2)}")
-    return result.GetSolution(x_2)
-
-def argmin_x3(x_1, x_2, z1_1, z3_1, z1_2, z2_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    x_3 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"x3 update: Is solved successfully: {result.is_success()}")
-    print(f"x3 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"x3 update: x3 optimal value: {result.GetSolution(x_3)}")
-    return result.GetSolution(x_3)
-
-def argmin_z1_1(x_1, x_2, x_3, z3_1, z1_2, z2_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    z1_1 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"z1_1 update: Is solved successfully: {result.is_success()}")
-    print(f"z1_1 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"z1_1 update: z1_1 optimal value: {result.GetSolution(z1_1)}")
-    return result.GetSolution(z1_1)
-
-def argmin_z3_1(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    z3_1 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"z3_1 update: Is solved successfully: {result.is_success()}")
-    print(f"z3_1 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"z3_1 update: z3_1 optimal value: {result.GetSolution(z3_1)}")
-    return result.GetSolution(z3_1)
-
-def argmin_z1_2(x_1, x_2, x_3, z1_1, z3_1, z2_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    z1_2 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"z1_2 update: Is solved successfully: {result.is_success()}")
-    print(f"z1_2 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"z1_2 update: z1_2 optimal value: {result.GetSolution(z1_2)}")
-    return result.GetSolution(z1_2)
-
-def argmin_z2_2(x_1, x_2, x_3, z1_1, z3_1, z1_2, z2_3, z3_3, y):
-    sub_prog = MathematicalProgram()
-    z2_2 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"z2_2 update: Is solved successfully: {result.is_success()}")
-    print(f"z2_2 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"z2_2 update: z2_2 optimal value: {result.GetSolution(z2_2)}")
-    return result.GetSolution(z2_2)
-
-def argmin_z2_3(x_1, x_2, x_3, z1_1, z3_1, z1_2, z2_2, z3_3, y):
-    sub_prog = MathematicalProgram()
-    z2_3 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"z2_3 update: Is solved successfully: {result.is_success()}")
-    print(f"z2_3 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"z2_3 update: z2_3 optimal value: {result.GetSolution(z2_3)}")
-    return result.GetSolution(z2_3)
-
-def argmin_z3_3(x_1, x_2, x_3, z1_1, z3_1, z1_2, z2_2, z2_3, y):
-    sub_prog = MathematicalProgram()
-    z3_3 = sub_prog.NewContinuousVariables(n)
-    sub_prog.AddCost(augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y))
-    result = Solve(prog)
-    print(f"z3_3 update: Is solved successfully: {result.is_success()}")
-    print(f"z3_3 update: optimal cost: {result.get_optimal_cost()}")
-    print(f"z3_3 update: z3_3 optimal value: {result.GetSolution(z3_3)}")
-    return result.GetSolution(z3_3)
-
+# Constraint values (x=z)
 A = np.array([[1,0,0],
                   [1,0,0],
                   [0,1,0],
@@ -250,31 +133,99 @@ A = np.array([[1,0,0],
                   [0,0,1],
                   [0,0,1]])
 B = -np.eye(6)
-c = 0
+c = np.zeros((6,1))
 
-rho=1
-iter_limit = 100
+def indicator_cost(f, x):
+    """
+    Return penalty * sum of constraint violations.
+
+    f is the constraint function.
+    x is a list of all variables fed to the constraint function.
+    """
+    penalty = 1e6
+    constraints = f(*x)
+    return 1e6*Evaluate(logical_and(*constraints))
+    print(f"violations: {violations}")
+    return 0
+
+indicator_cost(x1_constraint, np.array([[1]]))
+sub_prog = MathematicalProgram()
+var = sub_prog.NewContinuousVariables(1, "x1")
+indicator_cost(x1_constraint, [var])
+
+def augmented_lagrangian(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_1, z3_3, y):
+    x = np.vstack((x_1, x_2, x_3))
+    z = np.vstack((z1_1, z3_1, z1_2, z2_2, z2_3, z3_3))
+    
+    lagrangian = (x1_cost(x_1) + x2_cost(x_2) + x3_cost(x_3) + x1_x2_cost(z1_1, z1_2) + x2_x3_cost(z2_2, z2_3) + x1_x3_cost(z3_1, z3_3) +
+                  indicator_cost(x1_constraint, [x_1]) + indicator_cost(x2_constraint, [x_2]) + indicator_cost(x3_constraint, [x_3]) +
+                  indicator_cost(x1_x2_constraint, [x_1, x_2]) + indicator_cost(x2_x3_constraint, [x_2, x_3]) + indicator_cost(x1_x3_constraint, [x_1, x_3]) +
+                  y.T @ (A @ x + B @ z - c) +
+                  (rho/2) * ((A @ x + B @ z - c).T @ (A @ x + B @ z - c))
+                 )
+    return lagrangian[0][0]
+
+def argmin_variable(var_name, vars_dict, y):
+    sub_prog = MathematicalProgram()
+    var = sub_prog.NewContinuousVariables(n, var_name)
+    
+    # Update the variable to be optimized
+    vars_dict[var_name] = var
+
+    sub_prog.AddCost(augmented_lagrangian(**vars_dict, y=y))
+    result = Solve(sub_prog)
+    if not result.is_success():
+        print(f"Optimization for {var_name} failed.")
+    return result.GetSolution(var)
+
+rho = 1
+iter_limit = 0
 for i in range(iter_limit):
-    x_1 = argmin_x1(x_2, x_3, z1_1, z3_1, z1_2, z2_2, z2_3, z3_3, y)
+    fixed_vars = {
+        'x_1': x_1, 'x_2': x_2, 'x_3': x_3, 
+        'z1_1': z1_1, 'z1_2': z1_2, 'z2_2': z2_2, 
+        'z2_3': z2_3, 'z3_1': z3_1, 'z3_3': z3_3
+    }
     
-    x_2 = argmin_x2(x_1, x_3, z1_1, z3_1, z1_2, z2_2, z2_3, z3_3, y)
+    # Update x_1
+    x_1 = argmin_variable('x_1', fixed_vars, y)
+    fixed_vars['x_1'] = x_1
+        
+    # Update x_2
+    x_2 = argmin_variable('x_2', fixed_vars, y)
+    fixed_vars['x_2'] = x_2
     
-    x_3 = argmin_x3(x_1, x_2, z1_1, z3_1, z1_2, z2_2, z2_3, z3_3, y)
+    # Update x_3
+    x_3 = argmin_variable('x_3', fixed_vars, y)
+    fixed_vars['x_3'] = x_3
     
-    z1_1 = argmin_z1_1(x_1, x_2, x_3, z3_1, z1_2, z2_2, z2_3, z3_3, y)
+    # Update z1_1
+    z1_1 = argmin_variable('z1_1', fixed_vars, y)
+    fixed_vars['z1_1'] = z1_1
     
-    z3_1 = argmin_z3_1(x_1, x_2, x_3, z1_1, z1_2, z2_2, z2_3, z3_3, y)
+    # Update z1_2
+    z1_2 = argmin_variable('z1_2', fixed_vars, y)
+    fixed_vars['z1_2'] = z1_2
     
-    z1_2 = argmin_z1_2(x_1, x_2, x_3, z1_1, z3_1, z2_2, z2_3, z3_3, y)
+    # Update z2_2
+    z2_2 = argmin_variable('z2_2', fixed_vars, y)
+    fixed_vars['z2_2'] = z2_2
     
-    z2_2 = argmin_z2_2(x_1, x_2, x_3, z1_1, z3_1, z1_2, z2_3, z3_3, y)
+    # Update z2_3
+    z2_3 = argmin_variable('z2_3', fixed_vars, y)
+    fixed_vars['z2_3'] = z2_3
     
-    z2_3 = argmin_z2_3(x_1, x_2, x_3, z1_1, z3_1, z1_2, z2_2, z3_3, y)
+    # Update z3_1
+    z3_1 = argmin_variable('z3_1', fixed_vars, y)
+    fixed_vars['z3_1'] = z3_1
     
-    z3_3 = argmin_z3_3(x_1, x_2, x_3, z1_1, z3_1, z1_2, z2_2, z2_3, y)
+    # Update z3_3
+    z3_3 = argmin_variable('z3_3', fixed_vars, y)
+    fixed_vars['z3_3'] = z3_3
     
+    # Update dual variable y
     x = np.vstack((x_1, x_2, x_3))
     z = np.vstack((z1_1, z3_1, z1_2, z2_2, z2_3, z3_3))
     y = y + rho*(A @ x + B @ z - c)
     
-    print(f"x_{i}: {x}")
+    print(f"x_{i}: {x.flatten()}")
