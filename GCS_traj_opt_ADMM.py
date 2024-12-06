9db10df3bc92c0ea99cc92f37f7f0c91398aa61b
Michaelszeng <michaelszeng@gmail.com>
2024-12-06 02:10:57 -0500

    consensus manager class + first iteration of vertex update?

--- GCS_traj_opt_ADMM.py
+++ GCS_traj_opt_ADMM.py
@@ -23,12 +23,6 @@ V, E, I_v_in, I_v_out = build_graph(As, bs)
 print(f"V: {V}")
 print(f"E: {E}")
 
-# Variables to store current global values of split x and z variables
-#                      x_v          z_v         y_v
-x_global = np.zeros(2*n*len(V) + 2*n*len*(V) + len(V))
-#                                       x_v_e                                            z_v_e                             y_e
-z_global = np.zeros(sum(2*n*len((I_v_in[v] + I_v_out[v])) for v in V), sum(2*n*len((I_v_in[v] + I_v_out[v])) for v in V), len(E))
-
 # Public variable set to use to express consensus constraints and penalties
 x_v = {}
 z_v = {}
@@ -36,106 +30,159 @@ y_v = {}
 x_v_e = {}
 z_v_e = {}
 y_e = {}
-        
-def build_A_B_c_consensus_matrices(x_v, z_v, y_v, x_v_e, z_v_e, y_e):
+
+class ConsensusManager():
     """
-    A function designed to take advantage of Drake's built-in optimization 
-    tools to more easily express the consensus constraints and penalties in 
-    standard Ax + Bz = c form. Builds and returns the A, B, and c matrices 
-    for the consensus constraints for the given problem.
-    
-    Args:
-        Empty dictionaries for all variables in the split ADMM formulation.
-
-    Returns:
-        np.ndarray: A matrix.
-        np.ndarray: B matrix.
-        float: c vector.
+    A class designed to take advantage of Drake's built-in optimization tools to
+    more easily express the consensus constraints and penalties in standard
+    Ax + Bz = c form.
     """
-    def is_x_var(var):
-        return prog.FindDecisionVariableIndex(var) < z_idx
-    
-    # First, Build the MathematicalProgram containing all the variables in the split ADMM formulation.
-    prog = MathematicalProgram()
-    
-    # Build variable set for x variables
-    for v in V:
-        x_v[v] = prog.NewContinuousVariables(2 * n, f'x_{v}')
-        z_v[v] = prog.NewContinuousVariables(2 * n, f'z_{v}')
-        y_v[v] = prog.NewBinaryVariables(1, f'y_{v}')[0]
-    
-    # Build variable set for z variables
-    first_z_var = None
-    for v in V:
-        for e in I_v_in[v] + I_v_out[v]:
-            if first_z_var is None:
-                first_z_var = x_v_e[(v, e)]
-            x_v_e[(v, e)] = prog.NewContinuousVariables(2 * n, f'x_{v}_e_{e}')
-            z_v_e[(v, e)] = prog.NewContinuousVariables(2 * n, f'z_{v}_e_{e}')
-    for e in E:
-        y_e[e] = prog.NewBinaryVariables(1, f'y_e_{e}')[0]
+    def __init__(self, x_v, z_v, y_v, x_v_e, z_v_e, y_e):
+        """
+        Builds the MathematicalProgram containing all the variables in the split
+        ADMM formulation.
         
-    # Make it easier to index into the z variables
-    z_idx = prog.FindDecisionVariableIndex(first_z_var)
+        Args:
+            Empty dictionaries for all variables in the split ADMM formulation.
+        """    
+        self.prog = MathematicalProgram()
+        
+        # Build variable set for x variables
+        for v in V:
+            x_v[v] = self.prog.NewContinuousVariables(2 * n, f'x_{v}')
+            z_v[v] = self.prog.NewContinuousVariables(2 * n, f'z_{v}')
+            y_v[v] = self.prog.NewBinaryVariables(1, f'y_{v}')[0]
+        
+        # Build variable set for z variables
+        first_z_var = None
+        for v in V:
+            for e in I_v_in[v] + I_v_out[v]:
+                if first_z_var is None:
+                    first_z_var = x_v_e[(v, e)]
+                x_v_e[(v, e)] = self.prog.NewContinuousVariables(2 * n, f'x_{v}_e_{e}')
+                z_v_e[(v, e)] = self.prog.NewContinuousVariables(2 * n, f'z_{v}_e_{e}')
+        for e in E:
+            y_e[e] = self.prog.NewBinaryVariables(1, f'y_e_{e}')[0]
+            
+        # Make it easier to index into the z variables
+        self.z_idx = self.prog.FindDecisionVariableIndex(first_z_var)
+        
+        self.A = None
+        self.B = None
+        self.c = None
+        
+    def is_x_var(self, var):
+        return self.prog.FindDecisionVariableIndex(var) < self.z_idx
+        
+    def build_A_B_c_consensus_matrices(self):
+        """
+        Builds the A, B, and c matrices for the consensus constraints for the 
+        given problem.
+
+        Returns:
+            np.ndarray: A matrix.
+            np.ndarray: B matrix.
+            float: c vector.
+        """
+        # First, encode all the consensus constraints in the MathematicalProgram
+        consensus_costraints = []  # list of all consensus constraints
+        for e in E:
+            # Vertex-Edge consensus constraints
+            v, w = e
+            # x_v = x_v_e
+            consensus_costraints.append(x_v_e[(v, e)] == x_v[v])
+            # x_w = x_w_e
+            consensus_costraints.append(x_v_e[(w, e)] == x_v[w])
+            
+        for v in V:
+            # Flow and Perspective Flow consensus constraints
+            delta_sv = delta('s', v)
+            delta_tv = delta('t', v)
+
+            # y_v = sum_{e ∈ I_v_in} y_e + δ_{sv}
+            consensus_costraints.append(y_v[v] == sum(y_e[e] for e in I_v_in[v]) + delta_sv)
+            # y_v = sum_{e ∈ I_v_out} y_e + δ_{tv}
+            consensus_costraints.append(y_v[v] == sum(y_e[e] for e in I_v_out[v]) + delta_tv)
+            
+            for d in range(2*n):   # 2n because z_v is 2n-dimensional
+                # z_v = sum_in_z_v_e + δ_{sv} x_v
+                consensus_costraints.append(z_v[v][d] == sum(z_v_e[(v, e)][d] for e in I_v_in[v]) + delta_sv * x_v[v][d])
+                # z_v = sum_out_z_v_e + δ_{tv} x_v
+                consensus_costraints.append(z_v[v][d] == sum(z_v_e[(v, e)][d] for e in I_v_out[v]) + delta_tv * x_v[v][d])
+                
+        # Now, construct A, B, c
+        A = np.zeros((len(consensus_costraints), self.prog.num_vars()))
+        B = np.zeros((len(consensus_costraints), self.z.num_vars()))
+        c = np.zeros(len(consensus_costraints))
+        for i, formula in enumerate(consensus_costraints):
+            constraint_binding = self.prog.AddConstraint(formula)
+            constraint = constraint_binding.evaluator()  # Must be linear constraint
+            constraint_vars = constraint_binding.variables()
+            
+            # We assume the linear (equality) constraint given only has one row (i.e. only encodes a scalar equality constraint)
+            constraint_A = constraint.GetDenseA()[0]
+            constraint_b = constraint.upper_bound()[0]
+            assert constraint.lower_bound() == constraint.upper_bound()
+
+            for j, var in enumerate(constraint_vars):
+                var_idx = self.prog.FindDecisionVariableIndex(var)
+                
+                if self.is_x_var(var):
+                    A[i, var_idx] += constraint_A[j]
+                else:  # is z var
+                    B[i, var_idx - self.z_idx] += constraint_A[j]
+                
+                c[i] = constraint_b
+                
+        # Store for internal use later
+        self.A = A
+        self.B = B
+        self.c = c
+                
+        return A, B, c
     
+    def get_x_var_idx(self, var):
+        """
+        Find the index of the given variable in the x variable set.
+        """
+        idx = self.prog.FindDecisionVariableIndex(var)
+        assert idx < self.z_idx  # Ensure the variable is in the x variable set
+        return idx
     
-    # Then, encode all the consensus constraints in the MathematicalProgram
-    consensus_costraints = []  # list of all consensus constraints
-    for e in E:
-        # Vertex-Edge consensus constraints
-        v, w = e
-        # x_v = x_v_e
-        consensus_costraints.append(x_v_e[(v, e)] == x_v[v])
-        # x_w = x_w_e
-        consensus_costraints.append(x_v_e[(w, e)] == x_v[w])
-        
-    for v in V:
-        # Flow and Perspective Flow consensus constraints
-        delta_sv = delta('s', v)
-        delta_tv = delta('t', v)
-
-        # y_v = sum_{e ∈ I_v_in} y_e + δ_{sv}
-        consensus_costraints.append(y_v[v] == sum(y_e[e] for e in I_v_in[v]) + delta_sv)
-        # y_v = sum_{e ∈ I_v_out} y_e + δ_{tv}
-        consensus_costraints.append(y_v[v] == sum(y_e[e] for e in I_v_out[v]) + delta_tv)
-        
-        for d in range(2*n):   # 2n because z_v is 2n-dimensional
-            # z_v = sum_in_z_v_e + δ_{sv} x_v
-            consensus_costraints.append(z_v[v][d] == sum(z_v_e[(v, e)][d] for e in I_v_in[v]) + delta_sv * x_v[v][d])
-            # z_v = sum_out_z_v_e + δ_{tv} x_v
-            consensus_costraints.append(z_v[v][d] == sum(z_v_e[(v, e)][d] for e in I_v_out[v]) + delta_tv * x_v[v][d])
-            
-            
-    # Now, construct A, B, c
-    A = np.zeros((len(consensus_costraints), prog.num_vars()))
-    B = np.zeros((len(consensus_costraints), z.num_vars()))
-    c = np.zeros(len(consensus_costraints))
-    for i, formula in enumerate(consensus_costraints):
-        constraint_binding = prog.AddConstraint(formula)
-        constraint = constraint_binding.evaluator()  # Must be linear constraint
-        constraint_vars = constraint_binding.variables()
-        
-        # We assume the linear (equality) constraint given only has one row (i.e. only encodes a scalar equality constraint)
-        constraint_A = constraint.GetDenseA()[0]
-        constraint_b = constraint.upper_bound()[0]
-        assert constraint.lower_bound() == constraint.upper_bound()
-
-        for j, var in enumerate(constraint_vars):
-            var_idx = prog.FindDecisionVariableIndex(var)
-            
-            if is_x_var(var):
-                A[i, var_idx] += constraint_A[j]
-            else:  # is z var
-                B[i, var_idx - z_idx] += constraint_A[j]
-            
-            c[i] = constraint_b
-            
-    return A, B, c
-
-A, B, c = build_A_B_c_consensus_matrices(x_v, z_v, y_v, x_v_e, z_v_e, y_e)
+    def get_z_var_idx(self, var):
+        """
+        Find the index of the given variable in the z variable set.
+        """
+        idx = self.prog.FindDecisionVariableIndex(var) - self.z_idx
+        assert idx >= 0  # Ensure the variable is in the z variable set
+        return idx
+    
+    def get_num_x_vars(self):
+        return self.z_idx
+    
+    def get_num_z_vars(self):
+        return self.prog.num_vars() - self.z_idx
+    
+    def get_num_mu_vars(self):
+        if self.A is None:
+            raise ValueError("Call build_A_B_c_consensus_matrices() first.")
+        return self.A.shape[0]  # Number of rows of A = number of consensus constraints = number of mu variables
 
 
-def vertex_update(rho, v, x_v_e, z_v_e, y_e, mu):
+# Build consensus manager to handle construction of A, B, c matrices for consensus constraints and penalties
+consensus_manager = ConsensusManager(x_v, z_v, y_v, x_v_e, z_v_e, y_e)
+A, B, c = consensus_manager.build_A_B_c_consensus_matrices()  # Just build these once; they remain constant throughout optimization
+
+# Variables to store current global values of split x and z variables
+# Consists of x_v, z_v, y_v
+x_global = np.zeros(consensus_manager.get_num_x_vars())
+# Consists of x_v_e, z_v_e, y_e
+z_global = np.zeros(consensus_manager.get_num_z_vars())
+
+mu_global = np.zeros(consensus_manager.get_num_mu_vars())
+
+
+def vertex_update(rho, v):
     """
     Perform vertex update ("x-update") step for a single vertex v.
     
@@ -158,12 +205,13 @@ def vertex_update(rho, v, x_v_e, z_v_e, y_e, mu):
     prog.AddL2NormCost(A, b, np.hstack([z_v1, z_v2]))
     
     # Concensus Constraint Penalty: (rho/2) * ||Ax + Bz + mu||^2
-    # Define x vector that contain fixed valus for all but the variables corresponding to v
-    
-    prog.AddCost((rho/2) * (A @ x + B @ z_global - c + mu).T @ (A @ x + B @ z_global - c + mu))
-    
-    
-    prog.AddCost((rho/2) * ((sum(x_v - x_v_e[i] for i in range(len(x_v_e))) + mu).T @ (sum(x_v - x_v_e[i] for i in range(len(x_v_e))) + mu)))
+    # Define x vector that contain fixed values for all but the variables corresponding to v
+    x = x_global.copy()
+    x[consensus_manager.get_x_var_idx(x_v)] = x_v
+    x[consensus_manager.get_x_var_idx(z_v)] = z_v
+    x[consensus_manager.get_x_var_idx(y_v)] = y_v
+    # z and mu are all fixed
+    prog.AddCost((rho/2) * (A @ x + B @ z_global - c + mu_global).T @ (A @ x + B @ z_global - c + mu_global))
     
     # Point containment costraints
     m = As[v].shape[0]
